d913c20d341e458da9ecf59e7a5f875b298d79cc
Pablo Stafforini <pablo@stafforini.com>
2026-03-05 19:33:56 -0300

    Also compile .texi to .info on save in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,10 @@
 ((org-mode . ((eval . (add-hook 'after-save-hook
                                 (lambda ()
                                   (require 'ox-texinfo)
-                                  (let ((inhibit-message t))
-                                    (org-texinfo-export-to-texinfo)))
+                                  (let* ((inhibit-message t)
+                                         (texi (org-texinfo-export-to-texinfo))
+                                         (info (concat (file-name-sans-extension texi) ".info")))
+                                    (when (executable-find "makeinfo")
+                                      (call-process "makeinfo" nil nil nil
+                                                    texi "-o" info))))
                                 nil t)))))
